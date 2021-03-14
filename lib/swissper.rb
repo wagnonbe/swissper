b22cc5b30d2d7ecc60e36cbb7c51c9de46813647
Ben Wagnon <benbw1@gmail.com>
2021-03-14 15:12:28 -0400

    accept single sided values, add side delta value to pairing calc

--- lib/swissper.rb
+++ lib/swissper.rb
@@ -11,8 +11,11 @@ module Swissper
   class Pairer
     def initialize(options = {})
       @delta_key = options[:delta_key] || :delta
+      @side_delta_key = options[:side_delta_key] || :side_delta
       @exclude_key = options[:exclude_key] || :exclude
-      @bye_delta = options[:bye_delta] || -1
+      @bye_delta = options[:bye_delta] || -1,
+      @score_factor = options[:score_factor] || 1
+      @single_sided =  options[:single_sided] || false
     end
 
     def pair(player_data)
@@ -24,7 +27,7 @@ module Swissper
 
     private
 
-    attr_reader :delta_key, :exclude_key, :bye_delta
+    attr_reader :delta_key, :side_delta_key, :exclude_key, :bye_delta, :score_factor
 
     def graph
       edges = [].tap do |e|
@@ -42,7 +45,18 @@ module Swissper
     end
 
     def delta(a, b)
-      0 - (delta_value(a) - delta_value(b))**2
+      0 - delta_factor(a,b) - side_delta_factor(a, b)
+    end
+
+    def delta_factor(a, b)
+      (delta_value(a) - delta_value(b))**2 * @score_factor
+    end
+
+    def side_delta_factor(a,b)
+      return 0 unless @single_sided
+      return 0 if side_delta_value(a) * side_delta_value(b) <= 0 # always return 0 if they aren't biased in the same direction
+
+      ([side_delta_value(a).abs, side_delta_value(b).abs].min*4)**3
     end
 
     def targets(player)
@@ -53,6 +67,11 @@ module Swissper
       return player.send(delta_key) if player.respond_to?(delta_key)
       return bye_delta if player == Swissper::Bye
 
+      0
+    end
+
+    def side_delta_value(player)
+      return player.send(side_delta_key) if player.respond_to?(side_delta_key)
       0
     end
 
--- swissper.gemspec
+++ swissper.gemspec
@@ -23,7 +23,7 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "graph_matching", "~> 0.0"
 
-  spec.add_development_dependency "bundler", "~> 1.14"
+  spec.add_development_dependency "bundler", "~> 2.2.14"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
   spec.add_development_dependency "pry", "~> 0.10"
